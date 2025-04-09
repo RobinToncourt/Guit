37960d75428fbb1fc12c0062c91dcd1b7c9e4efe
Robin TONCOURT <robin.toncourt@gmail.com>
2025-04-09 19:03:44 +0200

    Initial commit.

---
+++ src/guit.rs
@@ -0,0 +1,51 @@
+#[derive(Default)]
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+pub struct Guit {}
+
+impl Guit {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(
+                storage,
+                eframe::APP_KEY,
+            ).unwrap_or_default();
+        }
+
+        Self::default()
+    }
+}
+
+impl eframe::App for Guit {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                egui::widgets::global_theme_preference_buttons(ui);
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                powered_by_egui_and_eframe(ui);
+            });
+        });
+    }
+
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+}
+
+fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.spacing_mut().item_spacing.x = 0.0;
+        ui.label("Powered by ");
+        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        ui.label(" and ");
+        ui.hyperlink_to(
+            "eframe",
+            "https://github.com/emilk/egui/tree/master/crates/eframe",
+        );
+        ui.label(".");
+    });
+}
